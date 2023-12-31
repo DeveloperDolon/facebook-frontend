c41064bc70ea31b49f1f4d697ef6cff8d716c9e8
Dolon Roy <dolonr718@gmail.com>
2023-12-31 20:10:02 +0600

    login page design compleate commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,107 +1,67 @@
-import Image from "next/image";
-
 const page = () => {
   return (
-    <div>
-      <div>
-        <h1>facebook</h1>
-        <p>
-          Facebook helps you connect and share <br />
-          with the people in your life.
-        </p>
-      </div>
+    <div className="bg-[#f0f2f5]">
+      <div className="grid md:grid-cols-2 grid-cols-1 items-center md:mt-0 mt-20 min-h-screen max-w-6xl mx-auto">
+        <div>
+          <h1 className="text-blue-500 md:text-7xl text-5xl font-bold">
+            facebook
+          </h1>
+          <p className="md:text-3xl text-xl mt-5">
+            Facebook helps you connect and share <br />
+            with the people in your life.
+          </p>
+        </div>
 
-      <div>
-        <div className="w-full max-w-sm p-6 m-auto mx-auto bg-white rounded-lg shadow-md dark:bg-gray-800">
-
-          <form className="mt-6">
-            <div>
-              <label
-                htmlFor="username"
-                className="block text-sm text-gray-800 dark:text-gray-200"
-              >
-                Username
-              </label>
-              <input
-                type="text"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
-              />
-            </div>
-
-            <div className="mt-4">
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm text-gray-800 dark:text-gray-200"
-                >
-                  Password
-                </label>
-                <a
-                  href="#"
-                  className="text-xs text-gray-600 dark:text-gray-400 hover:underline"
-                >
-                  Forget Password?
-                </a>
+        <div>
+          <div className="w-full max-w-md p-6 pt-3 m-auto mx-auto bg-white rounded-lg shadow-md">
+            <form className="mt-2">
+              <div>
+                <input
+                  type="email"
+                  className="block w-full px-4 py-3 mt-2 text-gray-700 bg-white border rounded-lg  focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                  placeholder="Email address or phone number"
+                />
               </div>
 
-              <input
-                type="password"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
-              />
-            </div>
+              <div className="mt-4">
+                <input
+                  type="password"
+                  className="block w-full px-4 py-3 mt-2 text-gray-700 bg-white border rounded-lg focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                  placeholder="Password"
+                />
+              </div>
 
-            <div className="mt-6">
-              <button className="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
-                Sign In
+              <div className="mt-6">
+                <button className="w-full px-6 py-2.5 md:text-xl text-lg font-bold tracking-wide text-white transition-colors duration-300 transform bg-blue-500 rounded-lg focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+                  Log in
+                </button>
+              </div>
+            </form>
+
+            <div className="text-center">
+              <a
+                href="#"
+                className="md:text-sm text-xs text-center mx-auto text-blue-500 pt-4 block hover:underline"
+              >
+                Forgotten password?
+              </a>
+            </div>
+            <div className="border-b-2 mt-5">
+
+            </div>
+            <div className="mt-6 text-center">
+              <button className="px-6 py-2.5 md:text-lg mx-auto text-base font-semibold tracking-wide text-white transition-colors duration-300 transform bg-[#42b72a] rounded-lg focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+                Create new account
               </button>
             </div>
-          </form>
-
-          <div className="flex items-center justify-between mt-4">
-            <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/5"></span>
-
-            <a
-              href="#"
-              className="text-xs text-center text-gray-500 uppercase dark:text-gray-400 hover:underline"
-            >
-              or login with Social Media
-            </a>
-
-            <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/5"></span>
           </div>
 
-          <div className="flex items-center mt-6 -mx-2">
-            <button
-              type="button"
-              className="flex items-center justify-center w-full px-6 py-2 mx-2 text-sm font-medium text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:bg-blue-400 focus:outline-none"
-            >
-              <svg className="w-4 h-4 mx-2 fill-current" viewBox="0 0 24 24">
-                <path d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z"></path>
-              </svg>
-
-              <span className="hidden mx-2 sm:inline">Sign in with Google</span>
-            </button>
-
-            <a
-              href="#"
-              className="p-2 mx-2 text-sm font-medium text-gray-500 transition-colors duration-300 transform bg-gray-300 rounded-lg hover:bg-gray-200"
-            >
-              <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24">
-                <path d="M23.954 4.569c-.885.389-1.83.654-2.825.775 1.014-.611 1.794-1.574 2.163-2.723-.951.555-2.005.959-3.127 1.184-.896-.959-2.173-1.559-3.591-1.559-2.717 0-4.92 2.203-4.92 4.917 0 .39.045.765.127 1.124C7.691 8.094 4.066 6.13 1.64 3.161c-.427.722-.666 1.561-.666 2.475 0 1.71.87 3.213 2.188 4.096-.807-.026-1.566-.248-2.228-.616v.061c0 2.385 1.693 4.374 3.946 4.827-.413.111-.849.171-1.296.171-.314 0-.615-.03-.916-.086.631 1.953 2.445 3.377 4.604 3.417-1.68 1.319-3.809 2.105-6.102 2.105-.39 0-.779-.023-1.17-.067 2.189 1.394 4.768 2.209 7.557 2.209 9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63.961-.689 1.8-1.56 2.46-2.548l-.047-.02z"></path>
-              </svg>
-            </a>
+          <div>
+            <p className="md:text-sm font-light text-xs text-center pt-7">
+              <span className="font-semibold">Create a Page</span> for a celebrity, brand or business.
+            </p>
           </div>
 
-          <p className="mt-8 text-xs font-light text-center text-gray-400">
-            {" "}
-            Don&apos;t have an account?{" "}
-            <a
-              href="#"
-              className="font-medium text-gray-700 dark:text-gray-200 hover:underline"
-            >
-              Create One
-            </a>
-          </p>
         </div>
       </div>
     </div>
