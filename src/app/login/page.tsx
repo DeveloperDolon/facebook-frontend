cdd731c6bb8d3b0697766c503c928b31b80760af
Dolon Roy <dolonr718@gmail.com>
2024-01-04 17:57:45 +0600

    create new account modal added commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,6 +5,16 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import { useState } from "react";
+import {
+  FormControl,
+  FormControlLabel,
+  InputLabel,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+  TextField,
+} from "@mui/material";
 
 const style = {
   position: "absolute",
@@ -14,7 +24,7 @@ const style = {
   bgcolor: "background.paper",
   boxShadow: 24,
   p: 4,
-  borderRadius: "10px"
+  borderRadius: "10px",
 };
 
 const page: React.FC = () => {
@@ -24,6 +34,12 @@ const page: React.FC = () => {
 
   const handleCreateAccount = () => {
     alert("hello world!");
+  };
+
+  const [age, setAge] = useState<string>("");
+
+  const handleChange = (event: object) => {
+    setAge(event.target.value);
   };
 
   return (
@@ -59,9 +75,7 @@ const page: React.FC = () => {
               </div>
 
               <div className="mt-6">
-                <button
-                  className="w-full px-6 py-2.5 md:text-xl text-lg font-bold tracking-wide text-white transition-colors duration-300 transform bg-blue-500 rounded-lg focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50 "
-                >
+                <button className="w-full px-6 py-2.5 md:text-xl text-lg font-bold tracking-wide text-white transition-colors duration-300 transform bg-blue-500 rounded-lg focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50 ">
                   Log in
                 </button>
               </div>
@@ -77,7 +91,6 @@ const page: React.FC = () => {
             </div>
             <div className="border-b-2 mt-5"></div>
             <div className="mt-6 text-center">
-
               <div>
                 <button
                   className="px-6 py-2.5 md:text-lg mx-auto text-base font-semibold tracking-wide text-white transition-colors duration-300 transform bg-[#42b72a] rounded-lg focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
@@ -92,20 +105,128 @@ const page: React.FC = () => {
                   aria-labelledby="modal-modal-title"
                   aria-describedby="modal-modal-description"
                 >
-                  <Box sx={style}>
-                    <Typography
-                      id="modal-modal-title"
-                      sx={{fontWeight: 600}}
-                      variant="h4"
-                      component="h2"
-                    >
-                      Sign Up
-                    </Typography>
-                    <Typography id="modal-mod
-                    al-description" sx={{ mt: 0 }}>
-                      It&apos;s quick and easy.
-                    </Typography>
-                  </Box>
+                  <form>
+                    <Box sx={style}>
+                      <Typography
+                        id="modal-modal-title"
+                        sx={{ fontWeight: 600 }}
+                        variant="h4"
+                        component="h2"
+                      >
+                        Sign Up
+                      </Typography>
+                      <Typography id="modal-mod al-description" sx={{ mt: 0 }}>
+                        It&apos;s quick and easy.
+                      </Typography>
+
+                      <p className="border-b-2 pt-2 mb-5"></p>
+
+                      <div className="grid grid-cols-2 gap-5">
+                        <TextField
+                          id="outlined-multiline-flexible"
+                          label="First name"
+                          maxRows={2}
+                        />
+                        <TextField
+                          id="outlined-multiline-flexible"
+                          label="Surname"
+                          maxRows={2}
+                        />
+                        <TextField
+                          className="col-span-2"
+                          id="outlined-multiline-flexible"
+                          label="Mobile number or email address"
+                          maxRows={2}
+                        />
+                        <TextField
+                          className="col-span-2"
+                          id="outlined-multiline-flexible"
+                          label="New password"
+                          type="password"
+                          maxRows={2}
+                        />
+                      </div>
+
+                      <p className="md:text-sm text-xs pt-5">Date of birth</p>
+                      <div className="grid grid-cols-3 gap-4">
+                        <FormControl fullWidth>
+                          <InputLabel id="demo-simple-select-label">
+                            Age
+                          </InputLabel>
+                          <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={age}
+                            label="Age"
+                            onChange={handleChange}
+                          >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                          </Select>
+                        </FormControl>
+                        <FormControl fullWidth>
+                          <InputLabel id="demo-simple-select-label">
+                            Age
+                          </InputLabel>
+                          <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={age}
+                            label="Age"
+                            onChange={handleChange}
+                          >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                          </Select>
+                        </FormControl>
+                        <FormControl fullWidth>
+                          <InputLabel id="demo-simple-select-label">
+                            Age
+                          </InputLabel>
+                          <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={age}
+                            label="Age"
+                            onChange={handleChange}
+                          >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                          </Select>
+                        </FormControl>
+                      </div>
+                      <p className="md:text-sm text-xs pt-5">Gender</p>
+
+                      <div>
+                        <RadioGroup
+                          className=""
+                          aria-labelledby="demo-radio-buttons-group-label"
+                          defaultValue="female"
+                          name="radio-buttons-group"
+                        ><div className="grid grid-cols-3">
+                            <FormControlLabel
+                              value="female"
+                              control={<Radio />}
+                              label="Female"
+                            />
+                            <FormControlLabel
+                              value="male"
+                              control={<Radio />}
+                              label="Male"
+                            />
+                            <FormControlLabel
+                              value="others"
+                              control={<Radio />}
+                              label="Others"
+                            />
+                        </div>
+                        </RadioGroup>
+                      </div>
+                    </Box>
+                  </form>
                 </Modal>
               </div>
             </div>
