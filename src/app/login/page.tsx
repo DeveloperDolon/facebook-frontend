7973fd10eb5d90969e2515fef56c0283ee6cb53c
Dolon Roy <dolonr718@gmail.com>
2024-01-18 19:44:41 +0600

    password show and hide system added commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
-import { useState } from "react";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import React from "react";
 import {
   FormControl,
   FormControlLabel,
@@ -28,28 +28,34 @@ const style = {
 };
 
 const page: React.FC = () => {
-  const [open, setOpen] = useState<boolean>(false);
+  const [open, setOpen] = React.useState<boolean>(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [show, setShow] = React.useState<boolean>(false);
 
   const handleCreateAccount = () => {
     alert("hello world!");
   };
 
-  const [age, setAge] = useState<string>("");
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [age, setAge] = React.useState<string>("");
 
   const handleChange = (event: object) => {
     setAge(event.target.value);
   };
 
+  const handleShowPassword = () => {
+    setShow(!show);
+  }
+
   return (
-    <div className="bg-[#f0f2f5]">
-      <div className="grid md:grid-cols-2 grid-cols-1 items-center md:mt-0 mt-20 min-h-screen max-w-6xl mx-auto">
-        <div>
+    <div className="bg-[#f0f2f5] ">
+      <div className="grid md:grid-cols-2 grid-cols-1 items-center md:mt-0 pt-20 min-h-screen overflow-scroll max-w-6xl mx-auto">
+        <div className="sm:text-left text-center">
           <h1 className="text-blue-500 md:text-7xl text-5xl font-bold">
             facebook
           </h1>
-          <p className="md:text-3xl text-xl mt-5">
+          <p className="md:text-3xl text-xl mt-5 sm:pb-0 pb-8">
             Facebook helps you connect and share <br />
             with the people in your life.
           </p>
@@ -65,13 +71,14 @@ const page: React.FC = () => {
                   placeholder="Email address or phone number"
                 />
               </div>
-
-              <div className="mt-4">
+              
+              <div className="mt-4 relative">
                 <input
-                  type="password"
                   className="block w-full px-4 py-3 mt-2 text-gray-700 bg-white border rounded-lg focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                  type={show ? "text" : "password"}
                   placeholder="Password"
                 />
+                <VisibilityIcon onClick={handleShowPassword} className="absolute right-3 top-3 cursor-pointer"></VisibilityIcon>
               </div>
 
               <div className="mt-6">
@@ -99,7 +106,7 @@ const page: React.FC = () => {
                   Create new account
                 </button>
                 <Modal
-                  className="rounded-lg"
+                  className="rounded-lg sm:w-auto w-full overflow-y-scroll"
                   open={open}
                   onClose={handleClose}
                   aria-labelledby="modal-modal-title"
@@ -121,7 +128,7 @@ const page: React.FC = () => {
 
                       <p className="border-b-2 pt-2 mb-5"></p>
 
-                      <div className="grid grid-cols-2 gap-5">
+                      <div className="grid sm:grid-cols-2 grid-cols-1 gap-5">
                         <TextField
                           id="outlined-multiline-flexible"
                           label="First name"
@@ -133,13 +140,13 @@ const page: React.FC = () => {
                           maxRows={2}
                         />
                         <TextField
-                          className="col-span-2"
+                          className="sm:col-span-2"
                           id="outlined-multiline-flexible"
                           label="Mobile number or email address"
                           maxRows={2}
                         />
                         <TextField
-                          className="col-span-2"
+                          className="sm:col-span-2"
                           id="outlined-multiline-flexible"
                           label="New password"
                           type="password"
@@ -148,7 +155,7 @@ const page: React.FC = () => {
                       </div>
 
                       <p className="md:text-sm text-xs pt-5">Date of birth</p>
-                      <div className="grid grid-cols-3 gap-4">
+                      <div className="grid md:grid-cols-3 grid-cols-1 gap-4">
                         <FormControl fullWidth>
                           <InputLabel id="demo-simple-select-label">
                             Age
@@ -206,7 +213,8 @@ const page: React.FC = () => {
                           aria-labelledby="demo-radio-buttons-group-label"
                           defaultValue="female"
                           name="radio-buttons-group"
-                        ><div className="grid grid-cols-3">
+                        >
+                          <div className="grid md:grid-cols-3 grid-cols-1">
                             <FormControlLabel
                               value="female"
                               control={<Radio />}
@@ -222,8 +230,17 @@ const page: React.FC = () => {
                               control={<Radio />}
                               label="Others"
                             />
-                        </div>
+                          </div>
                         </RadioGroup>
+
+                        <div className="text-center mt-4">
+                          <button
+                            className="px-10 py-2 md:text-base mx-auto text-sm font-semibold tracking-wide text-white transition-colors duration-300 transform bg-[#42b72a] rounded-lg focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
+                            onClick={handleOpen}
+                          >
+                            Sign Up
+                          </button>
+                        </div>
                       </div>
                     </Box>
                   </form>
